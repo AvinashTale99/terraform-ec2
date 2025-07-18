b95ee9d0faeb874f6c23f6627b9dfeb8b4905c1a
EC2 Default User <ec2-user@ip-172-31-8-121.ap-south-1.compute.internal>
2025-07-18 03:48:19 +0000

    code

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.4.0"
+    }
+  }
+}
+
+# main.tf
+resource "aws_instance" "web" {
+  ami           = "ami-0a1235697f4afa8a4"
+  instance_type = "t3.micro"
+  key_name      = "key"
+  tags = {
+    Name = "webserver"
+  }
+}
